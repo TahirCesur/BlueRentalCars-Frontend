126757a4b7f6dfcf9d9f6e9209f237e6f00954cb
Tahir Cesur <delenkar2825@gmail.com>
2021-12-01 16:54:34 +0300

    F01

--- bluerentalcars-frontend-master/src/components/common/TopBar.js
+++ bluerentalcars-frontend-master/src/components/common/TopBar.js
@@ -9,13 +9,17 @@ import {
   FiYoutube,
 } from "react-icons/fi";
 
+//* Telefon ve sosyal medya icon larını ekliyoruz...
+//* Bu yüzden 2 adet Col oluşturduk.. 20 ve 24. satırlara...
+
 const TopBar = () => {
   return (
     <div className="topbar">
       <Container>
         <Row>
           <Col xs={7}>
-            <FiPhoneCall size={16} /> <span className="d-none d-md-inline">CALL US</span> +1 235 98 95
+            <FiPhoneCall size={16} />{" "}
+            <span className="d-none d-md-inline">CALL US</span> +1 235 98 95
           </Col>
           <Col xs={5}>
             <ul>

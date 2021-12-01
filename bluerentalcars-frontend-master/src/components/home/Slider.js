c974f94f74a4d5f9b295c77300364ecb5e3ee1fb
Tahir Cesur <delenkar2825@gmail.com>
2021-12-01 17:43:59 +0300

    F01

--- bluerentalcars-frontend-master/src/components/home/Slider.js
+++ bluerentalcars-frontend-master/src/components/home/Slider.js
@@ -6,17 +6,20 @@ const Slider = () => {
   return (
     <div className="slider">
       <Container>
-          <Row>
-              <Col md={{span:7, order:"last"}}>
-                    <SliderCarousel/>
-              </Col>
-              <Col md={{span:5, order:"first"}}>
-                    FORM 
-              </Col>
-          </Row>
+        <Row>
+          <Col md={{ span: 7, order: "last" }}>
+            <SliderCarousel />
+          </Col>
+          <Col md={{ span: 5, order: "first" }}>FORM</Col>
+        </Row>
       </Container>
     </div>
   );
 };
 
 export default Slider;
+
+//* <Col md={{span:7, order:"last"}}>
+//! md 12 üzerinden carousel e 7 lik yer Form a 5 lik yer verdik.
+//! Ekran küçülünce order:"last" ve "first"
+//! yardımıyla soldaki sağa sağdaki sola geçmiş oluyr...

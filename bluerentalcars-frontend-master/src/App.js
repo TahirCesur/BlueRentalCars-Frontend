f55f8536fb138e59b5d7fd7bda5d96dd72527353
Tahir Cesur <delenkar2825@gmail.com>
2021-12-03 22:05:23 +0300

    F04

--- bluerentalcars-frontend-master/src/App.js
+++ bluerentalcars-frontend-master/src/App.js
@@ -1,23 +1,15 @@
 import React from "react";
 import Footer from "./components/common/Footer";
 import MenuBar from "./components/common/MenuBar";
-import Spacer from "./components/common/Spacer";
 import TopBar from "./components/common/TopBar";
-import Slider from "./components/home/Slider";
-import CustomerServices from "./components/services/CustomerServices";
-import Vehicles from "./components/services/Vehicles";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div>
       <TopBar />
       <MenuBar />
-      <Slider />
-      <Spacer />
-      <CustomerServices />
-      <Spacer />
-      <Vehicles />
-      <Spacer />
+      <HomePage />
       <Footer />
     </div>
   );

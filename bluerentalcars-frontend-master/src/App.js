578c5adfafd93ebe2575db190d223bce8b5a8ef2
Tahir Cesur <delenkar2825@gmail.com>
2021-12-04 00:48:14 +0300

    F04

--- bluerentalcars-frontend-master/src/App.js
+++ bluerentalcars-frontend-master/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/common/Footer";
 import MenuBar from "./components/common/MenuBar";
 import TopBar from "./components/common/TopBar";
-import HomePage from "./pages/HomePage";
-import ServicesPage from "./pages/ServicesPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import ServicesPage from "./pages/ServicesPage";
 
 const App = () => {
   return (

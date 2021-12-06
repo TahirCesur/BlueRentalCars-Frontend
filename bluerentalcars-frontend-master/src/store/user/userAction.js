41de7d023ff68112315a8309190801ae44c27b76
Tahir Cesur <delenkar2825@gmail.com>
2021-12-06 23:42:22 +0300

    F05

--- bluerentalcars-frontend-master/src/store/user/userAction.js
+++ bluerentalcars-frontend-master/src/store/user/userAction.js
@@ -13,7 +13,7 @@ export const loginFailed = () => ({
   type: types.LOGIN_FAILED,
 });
 
-/* User ın logout işleminde çağrılır */
+/* User ın logout çıkış işleminde çağrılır */
 export const logout = () => ({
   type: types.LOGOUT,
 });
